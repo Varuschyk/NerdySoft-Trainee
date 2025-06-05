e0d35e815c3af7c06403d1d5ec3b35aa88f5f651
O.Varushchyk <alexvarushik@gmail.com>
2025-06-05 13:06:24 +0300

    init commit

---
+++ nerdySoft-app/src/main/java/com/nerdysoft/nerdysoftapp/NerdySoftAppApplication.java
@@ -0,0 +1,13 @@
+package com.nerdysoft.nerdysoftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdySoftAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NerdySoftAppApplication.class, args);
+  }
+
+}
